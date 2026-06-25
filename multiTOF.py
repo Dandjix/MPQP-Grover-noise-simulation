afb33b5586402b6dbabef4fea390bd914d7dbb60
Timon <timondubreuil@outlook.com>
2026-06-25 18:56:08 +0200

    assertions for clarity

--- multiTOF.py
+++ multiTOF.py
@@ -7,54 +7,11 @@ import math
 import random
 import matplotlib.pyplot as plt
 
-def multiTOFNul(circuit : QCircuit, control_qubits):
-    if control_qubits == 1:
-        circuit.add(TOF([0,1],2))
-        return
-
-
-    def _multiTOF(circuit : QCircuit,control_qubits : int,work_qubits : int):
-        if work_qubits == 0:
-            return
-        
-        def _mutliTOFLeft(circuit : QCircuit,depth : int, work_qubits : int):
-            if depth == 0:
-                return
-            if depth == 1:
-                circuit.add(TOF([0,1],work_qubits+1))
-                return
-            
-            _mutliTOFLeft(circuit,depth-1,work_qubits)
-            circuit.add(TOF([depth + work_qubits -1,depth],depth + work_qubits))
-        
-        def _mutliTOFRight(circuit : QCircuit,depth : int, work_qubits : int):
-            if depth == 0:
-                return
-            if depth == 1:
-                circuit.add(TOF([0,1],work_qubits+1))
-                return
-            
-            circuit.add(TOF([depth + work_qubits -1,depth],depth + work_qubits))
-            _mutliTOFRight(circuit,depth-1,work_qubits)
-
-        
-        _mutliTOFLeft(circuit,work_qubits,work_qubits)
-        circuit.add(CNOT(control_qubits + work_qubits -1,control_qubits + work_qubits))
-        _mutliTOFRight(circuit,work_qubits,work_qubits)
-        return
-    
-    return _multiTOF(circuit,control_qubits,control_qubits-1)
-    
-
-
-
-
-
-
-
 def multiTOF(circuit : QCircuit, control_qubits : list[int], work_qubits : list[int], target : int):
+    assert len(control_qubits) >= 2, "A tofoli gate requires at least 2 control qubits";
+
     if len(work_qubits) == 0:
-        assert len(control_qubits) == 2, "If no work qubits given, there must be 2 control qubits"
+        assert len(control_qubits) == 2, f"If no work qubits given, there must be 2 control qubits. There are {len(control_qubits)} control qubits, so there should be {len(control_qubits) -1} work qubits."
         circuit.add(TOF([0,1],target))
         return
 
@@ -62,7 +19,7 @@ def multiTOF(circuit : QCircuit, control_qubits : list[int], work_qubits : list[
 
     qubits = control_qubits + work_qubits + [target]
 
-    def _multiTOF(circuit : QCircuit,control_qubits : int,work_qubits : int):
+    def _multiTOF(circuit : QCircuit,work_qubits : int):
         if work_qubits == 0:
             return
         
@@ -93,12 +50,10 @@ def multiTOF(circuit : QCircuit, control_qubits : list[int], work_qubits : list[
         return
         
     
-    return _multiTOF(circuit,control_qubits,work_qubits)
+    return _multiTOF(circuit,work_qubits)
     
     
 if __name__ == "__main__":
     circuit = QCircuit(11) 
-    multiTOF(circuit,[0,1,2],[3,4],10)
-    multiTOF(circuit,[0,1,2],[5,6],10)
-    multiTOF(circuit,[0,1],[],10)
+    multiTOF(circuit,[0,1,2,3],[],10)
     circuit.pretty_print()
