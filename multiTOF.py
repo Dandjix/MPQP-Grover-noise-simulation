490f8ab9634764e02257f315d249594e346fdbe6
Timon <timondubreuil@outlook.com>
2026-06-25 18:51:13 +0200

    multiTOF utilisable yay

--- multiTOF.py
+++ multiTOF.py
@@ -7,7 +7,7 @@ import math
 import random
 import matplotlib.pyplot as plt
 
-def multiTOF(circuit : QCircuit, control_qubits):
+def multiTOFNul(circuit : QCircuit, control_qubits):
     if control_qubits == 1:
         circuit.add(TOF([0,1],2))
         return
@@ -45,10 +45,60 @@ def multiTOF(circuit : QCircuit, control_qubits):
     
     return _multiTOF(circuit,control_qubits,control_qubits-1)
     
+
+
+
+
+
+
+
+def multiTOF(circuit : QCircuit, control_qubits : list[int], work_qubits : list[int], target : int):
+    if len(work_qubits) == 0:
+        assert len(control_qubits) == 2, "If no work qubits given, there must be 2 control qubits"
+        circuit.add(TOF([0,1],target))
+        return
+
+    assert len(work_qubits) == len(control_qubits) -1, f"wrong number of work qubits, should be {len(control_qubits) -1}"
+
+    qubits = control_qubits + work_qubits + [target]
+
+    def _multiTOF(circuit : QCircuit,control_qubits : int,work_qubits : int):
+        if work_qubits == 0:
+            return
+        
+        def _mutliTOFLeft(circuit : QCircuit,depth : int, work_qubits : int):
+            if depth == 0:
+                return
+            if depth == 1:
+                circuit.add(TOF([qubits[0],qubits[1]],qubits[work_qubits+1]))
+                return
+            
+            _mutliTOFLeft(circuit,depth-1,work_qubits)
+            circuit.add(TOF([qubits[depth + work_qubits -1],qubits[depth]],qubits[depth + work_qubits]))
+        
+        def _mutliTOFRight(circuit : QCircuit,depth : int, work_qubits : int):
+            if depth == 0:
+                return
+            if depth == 1:
+                circuit.add(TOF([qubits[0],qubits[1]],qubits[work_qubits+1]))
+                return
+            
+            circuit.add(TOF([qubits[depth + work_qubits -1],qubits[depth]],qubits[depth + work_qubits]))
+            _mutliTOFRight(circuit,depth-1,work_qubits)
+
+        
+        _mutliTOFLeft(circuit,len(work_qubits),len(work_qubits))
+        circuit.add(CNOT(work_qubits[-1],target))
+        _mutliTOFRight(circuit,len(work_qubits),len(work_qubits))
+        return
+        
+    
+    return _multiTOF(circuit,control_qubits,work_qubits)
+    
     
 if __name__ == "__main__":
-    circuit = QCircuit(10) # il faut un circuit deux fois plus grand que le nombre de bits dans 
-
-    multiTOF(circuit,5)
-
+    circuit = QCircuit(11) 
+    multiTOF(circuit,[0,1,2],[3,4],10)
+    multiTOF(circuit,[0,1,2],[5,6],10)
+    multiTOF(circuit,[0,1],[],10)
     circuit.pretty_print()
